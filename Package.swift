9da0b840d00d47447ff0cc64789f2d4c582a13ef
George Kye <iamkyegeorge@gmail.com>
2019-08-08 21:08:00 -0400

    add swift package manager support

--- Package.swift
+++ Package.swift
@@ -1,13 +1,28 @@
-//
-//  Package.swift
-//  SwiftLinkPreview
-//
-//  Created by Leonardo Cardoso on 04/07/2016.
-//  Copyright © 2016 leocardz.com. All rights reserved.
-//
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftLinkPreview"
+    name: "SwiftLinkPreview",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftLinkPreview",
+            targets: ["SwiftLinkPreview"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "SwiftLinkPreview",
+            path: "Sources"),
+        .testTarget(
+            name: "SwiftLinkPreviewTests",
+            dependencies: ["SwiftLinkPreview"],
+            path: "SwiftLinkPreviewTests"
+        ),
+    ]
 )
